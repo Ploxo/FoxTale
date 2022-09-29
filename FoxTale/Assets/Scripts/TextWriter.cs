925d63682cc1812acca35f4a00d94f2528598cce
Martin Quach <martin_quach@hotmail.com>
2022-09-29 12:02:22 +0200

    Textwriter effect
    Made a textwriter effect for dialogues.

---
+++ FoxTale/Assets/Scripts/TextWriter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextWriter : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textMeshProText;    //Grabs the Text Mesh Pro text component we want.
+
+    public string[] textArrays;
+
+    [SerializeField]
+    float timeBetweenCharacters;
+    
+    [SerializeField]
+    float timeForNextWords;
+
+    int sentence = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SentenceEndCheck();
+    }
+
+    void SentenceEndCheck()
+    {
+        if (sentence <= textArrays.Length - 1)
+        {
+            //Assign the new text in the array to the text object in TextMeshPro and start typing the new sentence.
+            textMeshProText.text = textArrays[sentence];
+            StartCoroutine(TextVisible());
+        }
+    }
+
+    private IEnumerator TextVisible()
+    {
+        textMeshProText.ForceMeshUpdate();  //Will force a regeneration of text for the text object? (This is neccessary according to the tutorial).
+        int totalVisibleCharacters = textMeshProText.textInfo.characterCount;   //Characters displaying will be the written message.
+        int counter = 0;    //Helps tracking the time.
+
+        while (true)
+        {
+            int visibleCount = counter % (totalVisibleCharacters + 1);  //visibleCount gets incremented as time goes.
+            textMeshProText.maxVisibleCharacters = visibleCount;
+
+            //Checks if the sentence is completed, if it is, feed the next sentence to the text writer.
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                sentence++;
+                Invoke("SentenceEndCheck", timeForNextWords);
+                break;
+            }
+
+            counter++;
+            yield return new WaitForSeconds(timeBetweenCharacters);
+        }
+    }
+
+}
